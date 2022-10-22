fcf887da28ed8f1d7cc5f84e870031f2794b1056
GabrielMendonca <gabriellmend51@gmail.com>
2022-10-21 21:51:13 -0300

    Primeira vez utilizando uma tela no terminal

--- main.cpp
+++ main.cpp
@@ -1,12 +1,44 @@
 #include <iostream> 
+#include <stdlib.h>
+#include <string>
+#include <curses.h>
+
+using namespace std;
 
 int main(){
 
-std::cout<<"Beta test"<<std::endl;
-std::cout<<"primeira interação"<<std::endl;
-std::cout<<"se aparecer deu certo"<<std::endl;
-std::cout<<"se aparecer deu certo"<<std::endl;
-std::cout<<"teste final"<<std::endl;
+    initscr();
+    noecho();
+    curs_set(0);
 
+        int yMax, xMax;
+        getmaxyx(stdscr, yMax, xMax);
+
+        WINDOW *win = newwin(yMax/2, xMax/2, yMax/4, xMax/4);
+        box(win, 0, 0);
+
+    mvwprintw(win, 0, 10, "Game");
+
+    char ch;
+    while(ch = wgetch(win)){
+        switch(ch){
+            case 'w':
+            wattron(win, A_STANDOUT); 
+            mvwprintw(win, 0, 2, "Termo");
+            wattroff(win, A_STANDOUT);
+            break;
+            case 'a':
+
+            break;
+            case 's':
+
+            break;
+            case 'd':
+
+            break;
+        }
+    }
+
+endwin();
 return 0;
 }
